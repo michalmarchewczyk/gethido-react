cd0e29107021b7d1b15a54c96ef75d8201ff1e02
Michał Marchewczyk <dev@marchewczyk.eu>
2020-07-09 13:03:50 +0200

    Fixed content container scrollbar

--- src/components/app/AppMain.js
+++ src/components/app/AppMain.js
@@ -13,7 +13,7 @@ import Settings from './Settings';
 
 
 const useStyles = makeStyles((theme) => ({
-    container: {
+    containerDiv: {
         marginTop: 56,
         maxHeight: 'calc(100vh - 56px)',
         height: 'calc(100vh - 56px)',
@@ -29,6 +29,8 @@ const useStyles = makeStyles((theme) => ({
         },
         overflow: 'hidden',
         overflowY: 'auto',
+    },
+    container: {
         width: 'auto',
         marginLeft: 60,
         '@media (min-width: 1400px)': {
@@ -77,46 +79,48 @@ function AppMain(props) {
                 stage={stage}
             />
             <LeftMenu toggleDrawer={toggleDrawer} drawer={drawer} className={classes.leftMenu}/>
-            <Container className={[classes.container, (drawer ? classes.containerOpen : '')].join(' ')} maxWidth='lg'>
-                <Switch>
-                    <Route path='/app/inbox'>
-                        <TasksList stage='inbox' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/trash'>
-                        <TasksList stage='trash' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/someday'>
-                        <TasksList stage='someday' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/reference'>
-                        <TasksList stage='reference' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/next'>
-                        <TasksList stage='next' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/waiting'>
-                        <TasksList stage='waiting' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/projects'>
-                        <TasksList stage='projects' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/calendar'>
-                        <TasksList stage='calendar' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/completed'>
-                        <TasksList stage='completed' setStage={setStage}/>
-                    </Route>
-                    <Route path='/app/profile'>
-                        <h1>Profile</h1>
-                    </Route>
-                    <Route path='/app/settings'>
-                        <Settings setStage={setStage}/>
-                    </Route>
-                    <Route path='/app'>
-                        <Redirect to='/app/inbox'/>
-                    </Route>
-                </Switch>
-            </Container>
+            <div className={classes.containerDiv}>
+                <Container className={[classes.container, (drawer ? classes.containerOpen : '')].join(' ')} maxWidth='lg'>
+                    <Switch>
+                        <Route path='/app/inbox'>
+                            <TasksList stage='inbox' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/trash'>
+                            <TasksList stage='trash' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/someday'>
+                            <TasksList stage='someday' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/reference'>
+                            <TasksList stage='reference' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/next'>
+                            <TasksList stage='next' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/waiting'>
+                            <TasksList stage='waiting' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/projects'>
+                            <TasksList stage='projects' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/calendar'>
+                            <TasksList stage='calendar' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/completed'>
+                            <TasksList stage='completed' setStage={setStage}/>
+                        </Route>
+                        <Route path='/app/profile'>
+                            <h1>Profile</h1>
+                        </Route>
+                        <Route path='/app/settings'>
+                            <Settings setStage={setStage}/>
+                        </Route>
+                        <Route path='/app'>
+                            <Redirect to='/app/inbox'/>
+                        </Route>
+                    </Switch>
+                </Container>
+            </div>
         </div>
     );
 }
